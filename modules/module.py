3ab27ba2aabb83e5238bb58a7f6f3b8d658cdcda
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2020-07-07 20:47:33 -0300

    Adicionando modulo generico para herança futura

---
+++ modules/module.py
@@ -0,0 +1,13 @@
+import os
+import inquirer
+import subprocess
+
+class Module :
+    def __init__ ( self, name, path ):
+        pass
+
+    def execute ( self, **kwargs ):
+        pass
+
+    def menu ( self ):
+        pass
